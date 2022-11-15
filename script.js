2f6f2054a33ae38e184065c9d63718d6e108c38c
2Xnavy <94471888+2Xnavy@users.noreply.github.com>
2022-11-15 15:13:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,34 @@ function script1(){
     console.log(all.join(', '));
 }
 
-const joinArrays = (arr1, arr2) => {
-    return [...arr1, ...arr2];
-}
+
+
+
+const joinArrays = (arr1, arr2) => [...arr1, ...arr2];
 
 let arr10 = ['seven','nine','eleven'];
 let arr20 = ['eight','ten','twelve'];
 
 console.log(joinArrays(arr10, arr20));
+
+
+
+
+const array1 = [1, 2, 3, 4];
+const initialValue = 10;
+const sumWithInitial = array1.reduce(
+  (previousValue, currentValue) => previousValue - currentValue,
+  initialValue
+);
+console.log(sumWithInitial);
+
+
+
+
+/*
+.map(), coś ze strzałkami: [1^2+3, 2^2+3, 3^2+3]
+.reduce()
+.filter()
+foreach
+*/
 
